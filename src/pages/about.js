d4363befbc978a416b1594e33c2eb8574912cd0b
RUYSEUNGHYUN <crs1211@naver.com>
2022-05-31 12:53:43 +0900

    about 페이지 추가

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+// markup
+const AboutPage = () => {
+  return (
+    <main>
+      <title>자기소개</title>
+      <h1>자기소개 입니다!!!</h1>
+      <div>하핳</div>
+    </main>
+  )
+}
+
+export default AboutPage
