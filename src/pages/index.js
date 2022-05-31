151a0b46432f5cf8e3dac38744ce2a9dc40c0951
RUYSEUNGHYUN <crs1211@naver.com>
2022-05-31 12:48:51 +0900

    커밋

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import * as React from "react"
+
+// markup
+const IndexPage = () => {
+  return (
+    <main>
+      <title>메인</title>
+      <h1>메인 페이지 입니다.</h1>
+    </main>
+  )
+}
+
+export default IndexPage
